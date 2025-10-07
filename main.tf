99738bb28a9232664b4124e06e050d58f20f21dc
ale.alencarr <alexandre-879@hotmail.com>
2025-10-07 14:22:20 -0300

    feat: deploy yml

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ resource "random_password" "password" {
 
 
 resource "azurerm_mssql_server" "sqlserver" {
-  name                         = "sql-tchungry-${random_string.suffix.result}"
+  name                         = "sql-tchungry-hnaia0"
   resource_group_name          = azurerm_resource_group.rg.name
   location                     = azurerm_resource_group.rg.location
   version                      = "12.0"
@@ -54,4 +54,19 @@ resource "azurerm_mssql_firewall_rule" "allow_azure_services" {
   server_id = azurerm_mssql_server.sqlserver.id  
   start_ip_address    = "0.0.0.0"
   end_ip_address      = "0.0.0.0"
+}
+
+
+resource "azurerm_storage_account" "storage" {
+  name                     = "st${replace(var.resource_group_name, "-", "")}" 
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"  
+}
+ 
+resource "azurerm_storage_container" "images" {
+  name                  = "imagens"  
+  storage_account_name  = azurerm_storage_account.storage.name
+  container_access_type = "blob"   
 }
--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,12 @@ output "db_connection_string" {
   value = "Server=tcp:${azurerm_mssql_server.sqlserver.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.sqldb.name};Persist Security Info=False;User ID=${azurerm_mssql_server.sqlserver.administrator_login};Password=${random_password.password.result};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
   sensitive = true
 }
+
+output "storage_account_connection_string" {
+  value     = azurerm_storage_account.storage.primary_connection_string
+  sensitive = true
+}
+
+output "storage_account_primary_blob_endpoint" {
+  value = azurerm_storage_account.storage.primary_blob_endpoint
+}
