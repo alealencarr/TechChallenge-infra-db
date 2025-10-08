f2ab293096a4cf264c649023476da218488d7f7c
ale.alencarr <alexandre-879@hotmail.com>
2025-10-08 16:52:38 -0300

    updated archives

--- main.tf
+++ main.tf
@@ -4,9 +4,13 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 3.0"
     }
-    random = {
-      source = "hashicorp/random"
-    }
+  }
+ 
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-state"
+    storage_account_name = "tfstatetchungryale"
+    container_name       = "tfstate"
+    key                  = "infra-data.tfstate"  
   }
 }
 
@@ -14,59 +18,46 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
+ 
+data "azurerm_resource_group" "rg" {
+  name = var.resource_group_name
 }
 
-resource "random_string" "suffix" {
-  length  = 6
-  special = false
-  upper   = false
-}
-
-resource "random_password" "password" {
-  length           = 16
-  special          = true
-  override_special = "!@#$%&"
-}
-
+ 
 
 resource "azurerm_mssql_server" "sqlserver" {
   name                         = "sql-tchungry-hnaia0"
-  resource_group_name          = azurerm_resource_group.rg.name
-  location                     = azurerm_resource_group.rg.location
+  resource_group_name          = data.azurerm_resource_group.rg.name 
+  location                     = data.azurerm_resource_group.rg.location 
   version                      = "12.0"
   administrator_login          = var.sql_admin_login
-  administrator_login_password = "R1Cz$lgv!mQNi6JE"
+  administrator_login_password = "R1Cz$lgv!mQNi6JE"  
 }
-
 
 resource "azurerm_mssql_database" "sqldb" {
   name      = "Hungry"
-  server_id = azurerm_mssql_server.sqlserver.id 
-  sku_name  = "S0"                             
+  server_id = azurerm_mssql_server.sqlserver.id
+  sku_name  = "S0"
 }
 
- 
 resource "azurerm_mssql_firewall_rule" "allow_azure_services" {
-  name      = "AllowAllWindowsAzureIps"
-  server_id = azurerm_mssql_server.sqlserver.id  
-  start_ip_address    = "0.0.0.0"
-  end_ip_address      = "0.0.0.0"
+  name             = "AllowAllWindowsAzureIps"
+  server_id        = azurerm_mssql_server.sqlserver.id
+  start_ip_address = "0.0.0.0"
+  end_ip_address   = "0.0.0.0"
 }
-
 
 resource "azurerm_storage_account" "storage" {
-  name                     = "st${replace(var.resource_group_name, "-", "")}" 
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"  
-}
  
+  name                     = "strgtchungryprod"
+  resource_group_name      = data.azurerm_resource_group.rg.name # Usa o 'data' source
+  location                 = data.azurerm_resource_group.rg.location # Usa o 'data' source
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
 resource "azurerm_storage_container" "images" {
-  name                  = "imagens"  
+  name                  = "imagens"
   storage_account_name  = azurerm_storage_account.storage.name
-  container_access_type = "blob"   
+  container_access_type = "blob"
 }
--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
-output "db_connection_string" {
-  value = "Server=tcp:${azurerm_mssql_server.sqlserver.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.sqldb.name};Persist Security Info=False;User ID=${azurerm_mssql_server.sqlserver.administrator_login};Password=${random_password.password.result};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+output "db_connection_string" { 
+  value = "Server=tcp:${azurerm_mssql_server.sqlserver.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.sqldb.name};Persist Security Info=False;User ID=${azurerm_mssql_server.sqlserver.administrator_login};Password=R1Cz$lgv!mQNi6JE;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
   sensitive = true
 }
 
